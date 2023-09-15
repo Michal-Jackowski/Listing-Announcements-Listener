1b5523706ed1e9557baed6a7f3f7c1dd90a98714
Michal-Jackowski <michaljacko@op.pl>
2023-09-15 21:13:15 +0200

    Kucoin Market Buy Function

--- ListingAnnouncementsListener.py
+++ ListingAnnouncementsListener.py
@@ -17,6 +17,7 @@ from kucoin.client import Client
 import kucoin_config
 import time
 import requests
+import ccxt
 
 # Telegram
 # Insert api_id here
@@ -44,25 +45,20 @@ API_secret_key_kucoin = kucoin_config.API_Secret_Key
 # Insert API passphrase here
 API_passphrase_kucoin = kucoin_config.API_Passphrase
 
-kucoin_client = Client(API_key_kucoin, API_secret_key_kucoin, API_passphrase_kucoin)
-# Sandbox is under maintenance
-#kucoin_client = Client(API_key_kucoin, API_secret_key_kucoin, API_passphrase, sandbox=True)
-
-#Example for get balance of accounts in python
-#Try CCXT instead of request
-url = 'https://api.kucoin.com/api/v1/accounts'
-now = int(time.time() * 1000)
-str_to_sign = str(now) + 'GET' + '/api/v1/accounts'
-signature = base64.b64encode(hmac.new(API_secret_key_kucoin.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
-passphrase = base64.b64encode(hmac.new(API_secret_key_kucoin.encode('utf-8'), API_passphrase_kucoin.encode('utf-8'), hashlib.sha256).digest())
-headers = {"KC-API-SIGN": signature, "KC-API-TIMESTAMP": str(now), "KC-API-KEY": API_key_kucoin, "KC-API-PASSPHRASE": passphrase, "KC-API-KEY-VERSION": "2"}
-response = requests.request('get', url, headers=headers)
-print(response.status_code)
-print(response.json())
+kucoin_exchange_authentification = {
+    'enableRateLimit': True,
+    'apiKey': API_key_kucoin,
+    'secret': API_secret_key_kucoin,
+    'password' : API_passphrase_kucoin
+}
+kucoin_client = ccxt.kucoin(kucoin_exchange_authentification)
 
 print('Listening started!')
-def open_web_browser_with_binance_page(y):
+def open_web_browser_with_binance_exchange_page(y):
     webbrowser.open("https://www.binance.com/en/trade/" + y + "?theme=dark&type=spot")
+
+def open_web_browser_with_kucoin_exchange_page(y):
+    webbrowser.open("https://www.kucoin.com/trade/" + y)
 
 def play_notification_sound():
     notification_sound_path = path.notification_sound_path
@@ -76,23 +72,28 @@ def filter_text(text):
         new_text = str(res[0]).replace("(", "")
         new_text2 = new_text.replace(")", "")
         print("Symbol to buy is " + new_text2)
-        buy_on_kucoin(new_text2)
-        return new_text2
+        #buy_on_kucoin(new_text2)
+        full_ticker = new_text2 + "-USDT"
+        open_web_browser_with_kucoin_exchange_page(full_ticker)
+        return 0
     
     # Coinbase Spot Search
-    # "🔵#Coinbase Asset
-    # ✨ VTHO VET" => Example
-
-    # Create Mind Map Scheme. Signal => Execution
+    if re.search("Coinbase Asset", text, re.IGNORECASE):
+        splited_text = text.split()
+        ticker = splited_text[3]
+        full_ticker = ticker + "-USDT"
+        buy_on_kucoin(full_ticker)
+        open_web_browser_with_kucoin_exchange_page(full_ticker)
+        return 0
 
     # Futures Search
     if re.search("Binance Futures Will Launch USDⓈ-M" and "Perpetual Contract" and "Leverage", text, re.IGNORECASE):
         splited_text = text.split()
         ticker = splited_text[6]
-        buy_on_binance(ticker)
-        return ticker
-    else:
-        return ""
+        #buy_on_binance(ticker)
+        full_ticker = ticker + "_USDT"
+        open_web_browser_with_binance_exchange_page(full_ticker)
+        return 0
     
 def buy_on_binance(x):
     # Get average price for x
@@ -107,11 +108,12 @@ def buy_on_binance(x):
     order = binance_client.create_order(symbol = x, side = binance_client.SIDE_BUY, type = binance_client.ORDER_TYPE_MARKET, quantity = max_buy_quantity)    
     print("Bought " + x + " on Binance")
 
-def buy_on_kucoin(x):
-    # place a market buy order
-    #order = kucoin_client.create_market_order(x, Client.SIDE_BUY, size=0.01)
-    #print(order)
-    print("Buy " + x + " on Kucoin Exchange!")
+def buy_on_kucoin(x):    
+    size = 0.015 # It is working :)
+
+    #kucoin_exchange.create_market_buy_order(symbol, size)
+    kucoin_client.create_market_sell_order(x, size)
+    print("Bough " + x + " on Kucoin Exchange!")
 
 # Listen do messages from target channel
 @client.on(events.NewMessage(chats=user_input_channel))
@@ -122,10 +124,8 @@ async def newMessageListener(event):
     # Get private telegram message
     # await client.forward_messages(entity='me', messages=event.message)
     print(newMessage)
-    ticker = filter_text(newMessage)
-    full_ticker = ticker + "_USDT"
+    filter_text(newMessage)
     play_notification_sound()
-    open_web_browser_with_binance_page(full_ticker)
 
 with client:
     client.run_until_disconnected()
